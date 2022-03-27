4c5241a77a9dd787a8dd29f3b486cea52b96a210
vivian <92966837+abigailviviannn@users.noreply.github.com>
2022-03-27 22:43:00 +0700

    update vivi

--- src/ATMprogram.java
+++ src/ATMprogram.java
@@ -78,7 +78,59 @@ public class ATMprogram {
             System.out.println("Sisa Saldo yang Anda miliki adalah sebesar Rp." + atm.getSaldo());
             transaksiLagi();
         }
-
+        //------------------------------------------ Menu penarikan
+        public void tarik() {
+            double penarikan;
+            Scanner scan = new Scanner(System.in);
+            System.out.println("=======================================================");
+            System.out.print("|");
+            System.out.print("\t            Penarikan Tunai                   |"
+                    + "                |");
+            System.out.println();
+            System.out.println("=======================================================");
+            System.out.print("|");
+            System.out.print("\t           > Pilih Nominal <                  |");
+            System.out.println();
+            System.out.print("|");
+            System.out.print("\t[1] Rp. 100.000          [2] Rp. 200.000      |");
+            System.out.println();
+            System.out.print("|");
+            System.out.print("\t[3] Rp. 500.000          [4] Rp. 1000.0000    |");
+            System.out.println();
+            System.out.print("|");
+            System.out.println("\t[5] Rp. 2000.0000        [6] Penarikan        |");
+            System.out.println("=======================================================");
+            System.out.print("\t         Masukan pilihan Anda : ");
+            pil = s.nextInt();
+            System.out.println("=======================================================");
+            switch (pil) {
+                case 1:
+                    tarikNominal(100000);
+                    break;
+                case 2:
+                    tarikNominal(200000);
+                    break;
+                case 3:
+                    tarikNominal(500000);
+                    break;
+                case 4:
+                    tarikNominal(1000000);
+                    break;
+                case 5:
+                    tarikNominal(2000000);
+                    break;
+                case 6:
+                    System.out.println("\t         Masukan Nominal : ");
+                    System.out.println("=======================================================");
+                    System.out.print("Jumlah Penarikan : ");
+                    penarikan = scan.nextDouble();
+                    tarikNominal(penarikan);
+                    break;
+                default:
+                    System.out.println("\tPilihan Yang Anda masukan salah");
+                    tarik();
+            }
+        }
 
 
     }
