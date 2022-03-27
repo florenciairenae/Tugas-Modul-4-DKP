41bfaf8679f221021cae59ea4f8b82e6e9eb4b8f
florenciairenae <102208764+florenciairenae@users.noreply.github.com>
2022-03-27 23:20:42 +0700

    Update ATMclass.java

--- src/ATMclass.java
+++ src/ATMclass.java
@@ -1,18 +1,3 @@
 public class ATMclass {
-    int pin;
-    double saldo;
 
-    public void setPin(int password) {
-        this.pin = password;
-    }
-    public void setSaldo(double saldo) {
-        this.saldo = saldo;
-    }
-
-    public int getPin() {
-        return pin;
-    }
-    public double getSaldo() {
-        return saldo;
-    }
 }
